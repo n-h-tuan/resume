966237fc034d4565f1b6cd27f30173342bfc7d15
Tuan Hoang <nguyenledly1997@gmail.com>
2022-09-27 14:49:04 -0400

    update

--- src/custom-pages/Layout/CursorShadow.vue
+++ src/custom-pages/Layout/CursorShadow.vue
@@ -6,9 +6,19 @@
 export default {
   mounted() {
     var shadow = document.getElementById("cursorShadow");
+    let xShadow = 0;
+    let yShadow = 0;
+    let scaleShadow = 1;
+    let velocity = 1;
     document.addEventListener("mousemove", (e) => {
-      shadow.style.left = e.pageX - 29 + "px";
-      shadow.style.top = e.pageY - 26 + "px";
+      // get velocity
+      velocity = Math.ceil((Math.abs(e.movementX) + Math.abs(e.movementY)) / 2);
+      scaleShadow = 1 + (velocity / 100);
+      // get cursor position
+      xShadow = e.x - 25;
+      yShadow = e.y - 25;
+      shadow.style.display = "block";
+      shadow.style.transform = `translate3d(${xShadow}px, ${yShadow}px, 0px) scale(${scaleShadow})`;
     });
   },
 };
@@ -16,13 +26,16 @@ export default {
 
 <style lang="scss" scope>
 .cursor-shadow {
-    pointer-events: none;
+  display: none;
+  pointer-events: none;
   width: 50px;
   height: 50px;
   background-color: rgba(59, 131, 59, 0.6);
-  position: absolute;
+  position: fixed;
+  top: 0;
+  left: 0;
   border-radius: 50%;
   transition: all 0.2s ease-out;
-  z-index: 10;
+  z-index: 99999;
 }
 </style>
