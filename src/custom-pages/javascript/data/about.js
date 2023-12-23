433de02b0dec4ccdad0ab60b1c988278648383b7
Hoang Tuan Nguyen <nguyen.h.tuan0306@gmail.com>
2023-12-23 13:59:53 -0500

    update: change address

--- src/custom-pages/javascript/data/about.js
+++ src/custom-pages/javascript/data/about.js
@@ -1,5 +1,5 @@
 export default {
-  address: "52 Cardill Crescent, Waterloo, Ontario N2L3Y8 Canada",
+  address: "Kitchener, Ontario, N2L 3Y8, Canada",
   phone: "(+1519)-897-3658",
   email: "nguyen.h.tuan1997@gmail.com"
 };
